f408d94511da3d1124992cab65c22c1674f518aa
Noah Sherwin <noahrsherwin@gmail.com>
2021-06-04 02:35:19 -0700

    Initial Commit
    feat(task): FileSystemDelete
    feat(task): FileSystemTakeOwnership
    feat(exception): PathIsNullException
    feat(exception): UnauthorizedAccessException

---
+++ README.md
@@ -0,0 +1,10 @@
+# The Intern
+#### A small library for menial labor. Designed for the .Net Framework.
+This library was created to handle simple tasks in an elevated process.
+
+
+TODO: 
+- Allow invokation via `%windir%/System32/RunDLL32.exe`.
+- Accept input from other processes without using startup arguments.
+- Define Exit Codes used to determine if greater authorization is required to complete a task.
+
---
+++ Tasks.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Intern
+{
+    public class Tasks
+    {
+        /// <summary>
+        ///     The Type of Task to execute.
+        /// </summary>
+        public enum Type
+        {
+            FileSystemDelete,
+            FileSystemTakeOwnership
+        }
+
+        /// <summary>
+        ///     Execute the specified task—with the given path if necessary.
+        /// </summary>
+        /// <param name="task">The <see cref="Type"> of task to execute.</param>
+        /// <param name="path">A network, filesystem, Window Registry path.</param>
+        public static void DoTask(Type task, string path = null)
+        {
+            switch (task)
+            {
+                case Type.FileSystemDelete:
+                    if (path == null) throw Exceptions.PathIsNullException(task);
+
+                    break;
+
+                case Type.FileSystemTakeOwnership:
+                    if (path == null) throw Exceptions.PathIsNullException(task);
+
+                    break;
+
+                default: throw new ArgumentOutOfRangeException($"The Intern does not recognize the task, \"{task}\".");
+            }
+        }
+
+        public static class Exceptions
+        {
+            public static ArgumentNullException PathIsNullException(Type task)
+            {
+                throw new ArgumentNullException($"The Intern must be provided with a file or directory path for the task \"{task}\".");
+            }
+
+            public static UnauthorizedAccessException UnauthorizedAccessException(Type task, string path)
+            {
+                throw new UnauthorizedAccessException($"The Intern was denied required permissions to \"{path}\" while working on the task, \"{task}\".");
+            }
+        }
+    }
+}
