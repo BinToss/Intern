35c76f00ca477d6e587829865c40ad82d32b27b8
Noah Sherwin <noahrsherwin@gmail.com>
2021-06-27 05:29:18 -0700

    feat(Memo): leverage the newfound flexibility of the Status class

--- Memo.cs
+++ Memo.cs
@@ -8,17 +8,18 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Xml;
 using static System.IO.Path;
+using static Intern.Status.Type;
 
 namespace Intern
 {
     public class Memo
     {
         /// <summary>
-        ///
+        ///     Write the tasks and their parameters to a temporary, XML-based Memo file.
         /// </summary>
-        /// <param name="taskList"></param>
-        /// <returns></returns>
-        public Status Write(List<Task> taskList)
+        /// <param name="taskList">The list of tasks to write to the Memo file.</param>
+        /// <returns>An instance of <see cref="Status"/></returns>
+        public static Status Write(List<Task> taskList)
         {
             var memoPath = GetTempFileName();
 
@@ -29,11 +30,14 @@ namespace Intern
                 /// TODO: Finish Memo.Write()
                 /// 1. Task list to XML
                 /// 2. Copy
-
+                ///
+                var serializer = new System.Xml.Serialization.XmlSerializer(typeof(List<Task>));
+                serializer.Serialize(ms, taskList);
+                ms.Position = 0;
                 ms.CopyTo(fs);
             }
 
-            return new Status(true, "Memo read successfully; Task started in background");
+            return new Status(Succeeded, "Memo read successfully; Task started in background");
         }
 
         /// TODO: Finish Memo.Read()
@@ -42,7 +46,7 @@ namespace Intern
         /// </summary>
         /// <param name="memoPath">The path the to memo file.</param>
         /// <returns>An instance of <see cref="Status"/>.</returns>
-        public Status Read(string memoPath)
+        public static Status Read(string memoPath)
         {
             bool readSuccess = false;
             bool tasksStarted = false;
@@ -73,11 +77,11 @@ namespace Intern
             catch (Exception e)
             {
                 if (!readSuccess)
-                    return new Status(false, "The Intern could not read the memo. Reason: " + e.Message);
+                    return new Status(Failed, "The Intern could not read the memo. Reason: " + e.Message, uri: new Uri(memoPath), e: e);
                 else if (!tasksStarted)
-                    return new Status(false, "The Intern could not start the task(s). Reason: " + e.Message);
+                    return new Status(Failed, "The Intern could not start the task(s). Reason: " + e.Message);
                 else
-                    return new Status(false, "The Intern failed to complete the task(s). Reason: " + e.Message);
+                    return new Status(Failed, "The Intern failed to complete the task(s). Reason: " + e.Message);
             }
 
             var list = new List<bool>
@@ -87,7 +91,7 @@ namespace Intern
             };
 
             /** Read succeeded */
-            return new Status(true, "The Intern read the memo and started the task(s).");
+            return new Status(Succeeded, "The Intern read the memo and started the task(s).");
         }
     }
 }
