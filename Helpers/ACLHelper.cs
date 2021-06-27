238018fb93d45fdf64b2f0595e3756485e6467f1
Noah Sherwin <noahrsherwin@gmail.com>
2021-06-27 05:32:39 -0700

    feat: Add barebones ACLHelper class

---
+++ Helpers/ACLHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.AccessControl;
+using System.Security.Permissions;
+
+namespace Intern.Helpers
+{
+    public class ACLComposite
+    {
+        public enum ObjectType
+        {
+            _unknown = 0,
+            Directory,
+            File,
+            Process,
+            RegistryKey
+        }
+
+        public FileIOPermission FileIOPermission(PermissionState state)
+        {
+            return new System.Security.Permissions.FileIOPermission(state);
+        }
+
+        public FileIOPermission IOPermission;
+
+        public object ACL = new System.Security.Permissions.FileIOPermission(PermissionState.None); // alternatively, PermissionState.Unrestricted
+    }
+}
